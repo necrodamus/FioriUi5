1b1ebc37fa77940c8ff5f4d94ac6712206333ca3
Necro Dev <diosymifamilia@gamil.com>
2021-05-13 11:55:30 -0300

    ej13

---
+++ ej13/view/app.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast",
+
+], function (Controller, MessageToast) {
+   "use strict";
+   return Controller.extend("sap.ui.demo.walkthrough.view.app", {
+
+
+      onShowHello: function () {
+         // read msg from i18n model
+         let oBundle = this.getView().getModel("i18n").getResourceBundle();
+         let sRecipient = this.getView().getModel().getProperty("/recipient/name");
+         let sMsg = oBundle.getText("helloMsg", [sRecipient]);
+         // Show msg 
+         MessageToast.show(sMsg);
+
+      }
+   });
+});
