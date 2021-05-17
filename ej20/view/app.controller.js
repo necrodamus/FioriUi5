070cadbaa66dd143bc55dc78c6e2300df71af352
Necro Dev <diosymifamilia@gamil.com>
2021-05-17 09:56:39 -0300

    ej 20 binding with json price

---
+++ ej20/view/app.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast",
+
+], function (Controller, MessageToast) {
+   "use strict";
+   return Controller.extend("sap.ui.demo.walkthrough.view.app", {
+      onOpenDialog : function () {
+			this.getOwnerComponent().openHelloDialog();
+		}
+   });
+});
